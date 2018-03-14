5c57f0e9d418c9293ee5ffda42732b03068768d6
Alex de Caso <alex@agiledevers.com>
2018-03-14 19:24:31 +0100

    refactor and clean code

--- addon/adapters/adapter.js
+++ addon/adapters/adapter.js
@@ -1,19 +1,20 @@
 import DS from 'ember-data';
 import { pluralize } from 'ember-inflector';
 import { dasherize } from '@ember/string';
+import { isArray } from '@ember/array';
 
 export default DS.JSONAPIAdapter.extend({
   urlForUpdateRecord(id, modelName, snapshot) {
     let { adapterOptions } = snapshot;
+    let originalUpdateURL = this._super(...arguments);
+
     if (this.urlForUpdateRelationship && adapterOptions) {
       return this.urlForUpdateRelationship(id, modelName, snapshot, adapterOptions.relationshipToUpdate);
     }
 
-    let originalUpdateURL = this._super(...arguments);
     if (adapterOptions && adapterOptions.relationshipToUpdate) {
       let { relationshipToUpdate } = adapterOptions;
-      // let relationship = this._getRelationship(relationshipToUpdate, snapshot);
-      // let path = this._normalizeRelationshipPath(relationship.type, relationship.kind);
+
       const path = dasherize(relationshipToUpdate);
       return `${originalUpdateURL}/relationships/${path}`;
     }
@@ -21,53 +22,11 @@ export default DS.JSONAPIAdapter.extend({
     return originalUpdateURL;
   },
 
-  serializeIdType(data) {
-    let json = {};
-    const id = data.id;
-    const type = pluralize(data.constructor.modelName);
-    if (id) {
-      json['id'] = id;
-    }
-    if (type) {
-      json['type'] = type;
-    }
-    return json;
-  },
-
-  createPayload(snapshot) {
-    let payload;
-    const { adapterOptions } = snapshot;
-    let { data } = adapterOptions;
-    const { relationshipToUpdate } = adapterOptions;
-    const relationship = this._getRelationship(relationshipToUpdate, snapshot);
-
-    if(relationship.kind === 'hasMany') {
-      payload = [];
-
-      if(data && data.length) {
-        data.forEach((objectModel) => {
-          payload.pushObject(this.serializeIdType(objectModel));
-        });
-      }
-      else {
-        payload.pushObject(this.serializeIdType(data));
-      }
-      return {
-        'data': payload
-      };
-    }
-    else if(relationship.kind === 'belongsTo' && data.length){
-      throw new Error('Invalid received data: Data is not allowed to add in a belongsTo relationship');
-    }
-    payload = this.serializeIdType(adapterOptions.data);
-    return payload;
-  },
-
   updateRecord(store, type, snapshot) {
     let { adapterOptions } = snapshot;
 
     if(adapterOptions && adapterOptions.requestType) {
-      const data = this.createPayload(snapshot);
+      const data = this._createPayload(snapshot);
       let url = this.buildURL(type.modelName, snapshot.id, snapshot, 'updateRecord');
       if(adapterOptions.requestType === 'createRelationship') {
         return this.ajax(url, "POST", { data })
@@ -98,11 +57,32 @@ export default DS.JSONAPIAdapter.extend({
 
     return relationshipDescriptor;
   },
-  _normalizeRelationshipPath(type, relationshipKind) {
-    if (relationshipKind === 'hasMany') {
-      return pluralize(type);
-    }
 
-    return type;
-  }
+  _serializeIdType(data) {
+    return {
+      id: data.id,
+      type: pluralize(data.constructor.modelName)
+    };
+  },
+
+  _createPayload(snapshot) {
+    let payload = [];
+    let { data } = snapshot.adapterOptions;
+    const { relationshipToUpdate } = snapshot.adapterOptions;
+    const relationship = this._getRelationship(relationshipToUpdate, snapshot);
+
+    if(relationship.kind === 'hasMany') {
+      if(isArray(data)) {
+        data.forEach((objectModel) => {
+          payload.pushObject(this._serializeIdType(objectModel));
+        });
+      } else {
+        payload.pushObject(this._serializeIdType(data));
+      }
+      return { 'data': payload };
+    }
+    else {
+      throw new Error('POST and DELETE methods are not allowed for belongsTo relationships');
+    }
+  },
 });
